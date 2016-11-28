f1aff67c6b65182125e0fa1a5f65a1d684f94fab
vladimir <scubavisions@gmail.com>
2016-11-29 00:10:45 +0100

    Add ola

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-  	render html: "Ola horhe!"
+  def ola
+  	render html: "Ola horhe, ovde vicde!"
   end
 end
